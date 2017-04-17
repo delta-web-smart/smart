66419c12855480310b3183197954ec78cf12d1e2
Furin Konstantin <furin.konstantin1988@gmail.com>
2017-04-17 14:50:41 +0400

    Добавление инклудных файлов для футера

---
+++ include/footer/copyrights.php
@@ -0,0 +1,2 @@
+<span class="copyright"><?=date("Y")?> © Смарт Тайер. Все права защищены.<br>e-mail: info+smarttyre.ru</span>
+<span class="cite-mail">Замечания и предложения по сайту:<br>feedback@smarttyre.ru</span>
--- local/templates/main_smart_tyre/footer.php
+++ local/templates/main_smart_tyre/footer.php
@@ -4,13 +4,14 @@
     <footer class="footer">
       <div class="in-footer">
         <div class="left-part">
-          <span class="copyright">2013 © Смарт Тайер. Все права защищены.<br>e-mail: info+smarttyre.ru</span>
-          <span class="cite-mail">Замечания и предложения по сайту:<br>feedback@smarttyre.ru</span>
+          <?
+            $APPLICATION->IncludeComponent("bitrix:main.include", "", array("AREA_FILE_SHOW" => "file", "PATH" => SITE_DIR."include/footer/copyrights.php"), false);
+          ?>
         </div>
         <div class="right-part">
-          <span>Наш адрес: г.Москва ул.Южнопортовая д.7</span>
-          <span>График работы: Пн.-Пт. с 9:00 до 21:00,<br>Сб. с 9:00 до 17:00.</span>
-          <span>Телефон: +7 (495) <b>551-51-52</b></span>
+          <?
+            $APPLICATION->IncludeComponent("bitrix:main.include", "", array("AREA_FILE_SHOW" => "file", "PATH" => SITE_DIR."include/footer/contacts.php"), false);
+          ?>
         </div>
       </div>
       <div class="footer-shadow"></div>
