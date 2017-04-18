f3e4f36f98993c082736c451fd7544c269342a5b
Furin Konstantin <furin.konstantin1988@gmail.com>
2017-04-18 14:15:49 +0400

    Правка стилей для активного пункта меню

--- include/catalog/products_list_main_page.php
+++ include/catalog/products_list_main_page.php
@@ -1,4 +1,11 @@
-<? foreach($arResult['ITEMS'] as $arItem):?>
+<? foreach($arResult['ITEMS'] as $productId=>$arItem):?>
+    <? 
+        if ($LABEL_FOR_SALE == "hit" || $LABEL_FOR_SALE == "viewed") {
+            $addBasketId = $productId;
+        } else {
+            $addBasketId = $arItem["ID"];
+        }
+    ?>
     <li>
         <a href="<?=$arItem["DETAIL_PAGE_URL"]?>" class="img-wrap">
             <img src="<?=$arItem["PICTURE"]["src"]?>" alt="<?=$arItem["NAME"]?>" title="<?=$arItem["NAME"]?>">
@@ -10,7 +17,14 @@
         <? if (!empty($arItem["PROPERTIES"]["CML2_ARTICLE"]["VALUE"])):?>
             <span class="article"><?=GetMessage("CML2_ARTICLE_TITLE");?> <?=$arItem["PROPERTIES"]["CML2_ARTICLE"]["VALUE"]?></span>
         <? endif;?>
-        <input type="text" class="spinner" value="0">
-        <button class="price-btn"><?=$arItem["PRICE_DISCOUNT_VALUE"]?><i>i</i></button>
+        <? if (!isset($arItem['OFFERS']) || empty($arItem['OFFERS'])):?>
+            <? if ($arItem["CAN_BUY"]):?>
+                <input type="text" class="spinner" value="<? if ($arParams['USE_PRODUCT_QUANTITY'] == "Y"):?><? echo $arItem['CATALOG_MEASURE_RATIO']; ?><?else:?>0<?endif;?>">
+                <button class="price-btn"><?=$arItem["PRICE_DISCOUNT_VALUE"]?><i>i</i></button>
+            <? endif;?>
+         <? else:?>
+            <input type="text" class="spinner" value="<? if ($arParams['USE_PRODUCT_QUANTITY'] == "Y"):?><? echo $arItem['CATALOG_MEASURE_RATIO']; ?><?else:?>0<?endif;?>">
+            <button class="price-btn"><?=$arItem["PRICE_DISCOUNT_VALUE"]?><i>i</i></button>
+         <? endif;?>
     </li>
 <? endforeach;?>
