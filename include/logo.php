071125fc1ebda773c6fc8eee386ebff0247b2d86
delta-webru <ilya@delta-web.ru>
2017-05-16 18:11:50 +0300

    Добавление ссылки на главную
    //

--- include/logo.php
+++ include/logo.php
@@ -1,7 +1,8 @@
 <?php
     if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
 ?>
-<a href="<?if($APPLICATION->GetCurPage(false) == "/"):?>#<?else:?>/<?endif;?>" class="logo-wrap">
+<a href="/" class="logo-wrap">
+//<a href="<?if($APPLICATION->GetCurPage(false) == "/"):?>#<?else:?>/<?endif;?>" class="logo-wrap">
   <img src="<?=MAIN_TEMPLATE_PATH?>img/logo-main.png" alt="">
   <div class="slogan">
     <span>Смарт Тайер</span>
